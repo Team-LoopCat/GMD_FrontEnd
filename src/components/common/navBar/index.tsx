89f0ae0d20fa24e1aac2b937f03594ab1f6bbece
hA2zy <128462388+hA2zy@users.noreply.github.com>
2023-12-26 13:39:58 +0900

    ✨ :: 웹페이지 이동
    이제 끝이다.. 잘가라

--- src/components/common/navBar/index.tsx
+++ src/components/common/navBar/index.tsx
@@ -1,56 +1,61 @@
-// Navigation.tsx
-import React, { useState } from 'react';
-import * as S from './style';
-import { ReactComponent as ListIcon } from '../../../assets/icon/list.svg';
-import { ReactComponent as CListIcon } from '../../../assets/icon/Clist.svg';
-import { ReactComponent as CGraphIcon } from '../../../assets/icon/Cgraph.svg';
-import { ReactComponent as GraphIcon } from '../../../assets/icon/Graph.svg';
-import { ReactComponent as AdditionIcon } from '../../../assets/icon/addition.svg';
-import { ReactComponent as CAdditionIcon } from '../../../assets/icon/CAddition.svg';
-import { ReactComponent as LogoIcon } from '../../../assets/icon/GMDLogo.svg';
-import { ReactComponent as Logout } from '../../../assets/icon/logout.svg';
+import React, { useState } from "react";
+import * as S from "./style";
+import { ReactComponent as ListIcon } from "../../../assets/icon/list.svg";
+import { ReactComponent as CListIcon } from "../../../assets/icon/Clist.svg";
+import { ReactComponent as CGraphIcon } from "../../../assets/icon/Cgraph.svg";
+import { ReactComponent as GraphIcon } from "../../../assets/icon/Graph.svg";
+import { ReactComponent as AdditionIcon } from "../../../assets/icon/addition.svg";
+import { ReactComponent as CAdditionIcon } from "../../../assets/icon/CAddition.svg";
+import { ReactComponent as LogoIcon } from "../../../assets/icon/GMDLogo.svg";
+import { ReactComponent as Logout } from "../../../assets/icon/logout.svg";
+import { useNavigate } from "react-router-dom";
 
 const Navbar: React.FC = () => {
-  const [activeIcon, setActiveIcon] = useState('graph');
+ const [activeIcon, setActiveIcon] = useState("graph");
+ const navigate = useNavigate();
 
-  const handleIconClick = (iconType: string) => {
-    setActiveIcon(iconType);
-  };
+ const handleIconClick = (iconType: string) => {
+  setActiveIcon(iconType);
+  if (iconType === "graph") {
+    navigate("/MainPage");
+  } else if (iconType === "list") {
+    navigate("/ListPage");
+  } else if (iconType === "addition") {
+    navigate("/CheckPage");
+  }
+ };
 
-  return (
-    <S.NavContainer>
-      <S.LogoContainer>
-        <LogoIcon />
-      </S.LogoContainer>
-      <div>
-        <S.NavIcon onClick={() => handleIconClick('graph')}>
-          {activeIcon === 'graph' && <S.HighlightBox />}
-          <S.IconWrapper>
-            {activeIcon === 'graph' ? <CGraphIcon /> : <GraphIcon />}
-            {activeIcon === 'graph'}
-          </S.IconWrapper>
-        </S.NavIcon>
-        <S.NavIcon onClick={() => handleIconClick('list')}>
-          {activeIcon === 'list' && <S.HighlightBox />}
-          <S.IconWrapper>
-            {activeIcon === 'list' ? <CListIcon /> : <ListIcon />}
-            {activeIcon === 'list'}
-          </S.IconWrapper>
-        </S.NavIcon>
-        <S.NavIcon onClick={() => handleIconClick('addition')}>
-          {activeIcon === 'addition' && <S.HighlightBox />}
-          <S.IconWrapper>
-            {activeIcon === 'addition' ? <CAdditionIcon /> : <AdditionIcon />}
-            {activeIcon === 'addition'}
-          </S.IconWrapper>
-        </S.NavIcon>
-      </div>
-      
-      <S.LogoutWrapper>
-        <Logout />
-      </S.LogoutWrapper>
-    </S.NavContainer>
-  );
+ return (
+  <S.NavContainer>
+    <S.LogoContainer>
+      <LogoIcon />
+    </S.LogoContainer>
+    <div>
+      <S.NavIcon onClick={() => handleIconClick("graph")}>
+        {activeIcon === "graph" && <S.HighlightBox />}
+        <S.IconWrapper>
+          {activeIcon === "graph" ? <CGraphIcon /> : <GraphIcon />}
+        </S.IconWrapper>
+      </S.NavIcon>
+      <S.NavIcon onClick={() => handleIconClick("list")}>
+        {activeIcon === "list" && <S.HighlightBox />}
+        <S.IconWrapper>
+          {activeIcon === "list" ? <CListIcon /> : <ListIcon />}
+        </S.IconWrapper>
+      </S.NavIcon>
+      <S.NavIcon onClick={() => handleIconClick("addition")}>
+        {activeIcon === "addition" && <S.HighlightBox />}
+        <S.IconWrapper>
+          {activeIcon === "addition" ? <CAdditionIcon /> : <AdditionIcon />}
+        </S.IconWrapper>
+      </S.NavIcon>
+    </div>
+
+    <S.LogoutWrapper>
+      <Logout />
+    </S.LogoutWrapper>
+  </S.NavContainer>
+ );
 };
 
 export default Navbar;
--- src/pages/ListPage/index.tsx
+++ src/pages/ListPage/index.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Link, Navigate } from 'react-router-dom'; 
-import * as Styled from './style'; // 스타일드 컴포넌트에 따른 import
-import Nav from '../../components/common/navBar';
-import { DelStudentModal } from '../../components/modal/DelStudentModal/index'
-import { DeviceSettings } from '../../components/modal/DeviceSettings/index'
-import { useNavigate } from 'react-router-dom';
-const navigate = useNavigate();
+import React, { useState, useEffect } from "react";
+import * as Styled from "./style"; // 스타일드 컴포넌트에 따른 import
+import Nav from "../../components/common/navBar";
+import { DelStudentModal } from "../../components/modal/DelStudentModal/index";
+import { DeviceSettings } from "../../components/modal/DeviceSettings/index";
+import { useNavigate } from "react-router-dom";
 
 interface Student {
   studentId: string;
@@ -14,18 +12,20 @@ interface Student {
   submitted: boolean;
 }
 
-const handleDeviceSettings = () => {
-    navigate('/DeviceSettings')
-}
-
-const handleDelStudentModal = () => {
-    navigate('/DelStudentModal')
-}
-
 const Dashboard: React.FC = () => {
   const [studentsData, setStudentsData] = useState<Student[]>([]);
-  const [searchText, setSearchText] = useState<string>('');
-  const [submissionStatus, setSubmissionStatus] = useState<string>('none');
+  const [searchText, setSearchText] = useState<string>("");
+  const [submissionStatus, setSubmissionStatus] = useState<string>("none");
+
+  const navigate = useNavigate();
+
+  const handleDeviceSettings = () => {
+    navigate("/DeviceSettings");
+  };
+
+  const handleDelStudentModal = () => {
+    navigate("/DelStudentModal");
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,14 +37,39 @@ const Dashboard: React.FC = () => {
 
         // 테스트용 플레이스홀더 데이터
         setStudentsData([
-          { studentId: '1101', name: '오송주', lockerNumber: '111', submitted: true },
-          { studentId: '1102', name: '김강민', lockerNumber: '110', submitted: false },
-          { studentId: '1103', name: '홍서은', lockerNumber: '109', submitted: true },
-          { studentId: '1104', name: '이태윤', lockerNumber: '108', submitted: false },
-          { studentId: '1105', name: '권민휘', lockerNumber: '107', submitted: true },
+          {
+            studentId: "1101",
+            name: "오송주",
+            lockerNumber: "111",
+            submitted: true,
+          },
+          {
+            studentId: "1102",
+            name: "김강민",
+            lockerNumber: "110",
+            submitted: false,
+          },
+          {
+            studentId: "1103",
+            name: "홍서은",
+            lockerNumber: "109",
+            submitted: true,
+          },
+          {
+            studentId: "1104",
+            name: "이태윤",
+            lockerNumber: "108",
+            submitted: false,
+          },
+          {
+            studentId: "1105",
+            name: "권민휘",
+            lockerNumber: "107",
+            submitted: true,
+          },
         ]);
       } catch (error) {
-        console.error('데이터 가져오기 오류:', error);
+        console.error("데이터 가져오기 오류:", error);
       }
     };
 
@@ -53,20 +78,20 @@ const Dashboard: React.FC = () => {
 
   const getStatusColor = (student: Student) => {
     if (student.submitted) {
-      if (student.lockerNumber === 'airplane') {
-        return '#A4D5F6'; // 공기계 의심
-      } else if (student.lockerNumber === 'overnight') {
-        return '#E2EFF7'; // 외박/자습
+      if (student.lockerNumber === "airplane") {
+        return "#A4D5F6"; // 공기계 의심
+      } else if (student.lockerNumber === "overnight") {
+        return "#E2EFF7"; // 외박/자습
       } else {
-        return '#5B819A'; // 제출
+        return "#5B819A"; // 제출
       }
     } else {
-      return '#8CC0E4'; // 미제출
+      return "#8CC0E4"; // 미제출
     }
   };
 
   const getStatusLength = (student: Student) => {
-    if (student.lockerNumber === 'airplane') {
+    if (student.lockerNumber === "airplane") {
       return 80; // 공기계 의심 길이
     } else if (student.submitted) {
       return 50; // 제출 길이
@@ -75,9 +100,12 @@ const Dashboard: React.FC = () => {
     }
   };
 
-  const filteredStudents = studentsData.filter((student) =>
-    student.name.toLowerCase().includes(searchText.toLowerCase()) &&
-    (submissionStatus === 'none' || (student.submitted && submissionStatus === 'submitted') || (!student.submitted && submissionStatus === 'unsubmitted'))
+  const filteredStudents = studentsData.filter(
+    (student) =>
+      student.name.toLowerCase().includes(searchText.toLowerCase()) &&
+      (submissionStatus === "none" ||
+        (student.submitted && submissionStatus === "submitted") ||
+        (!student.submitted && submissionStatus === "unsubmitted"))
   );
 
   return (
@@ -145,27 +173,35 @@ const Dashboard: React.FC = () => {
           <Styled.RightContainer>
             {filteredStudents.map((student) => (
               <Styled.RightItem key={student.studentId}>
-                <Styled.RightItemLabel>{student.studentId}</Styled.RightItemLabel>
+                <Styled.RightItemLabel>
+                  {student.studentId}
+                </Styled.RightItemLabel>
                 <Styled.RightItemLabel>{student.name}</Styled.RightItemLabel>
-                <Styled.RightItemLabel>{student.lockerNumber}</Styled.RightItemLabel>
+                <Styled.RightItemLabel>
+                  {student.lockerNumber}
+                </Styled.RightItemLabel>
                 <Styled.RightItemLabel>
                   {student.submitted
-                    ? '제출'
-                    : student.lockerNumber === 'airplane'
-                    ? '공기계 의심'
-                    : student.lockerNumber === 'overnight'
-                    ? '외박/자습'
-                    : '미제출'}
+                    ? "제출"
+                    : student.lockerNumber === "airplane"
+                    ? "공기계 의심"
+                    : student.lockerNumber === "overnight"
+                    ? "외박/자습"
+                    : "미제출"}
                   <Styled.RightStatusDot submitted={student.submitted} />
                 </Styled.RightItemLabel>
-                <Styled.RightItemLabel onClick={handleDeviceSettings}>디바이스 조회</Styled.RightItemLabel>
-                <Styled.RightItemLabel onClick={handleDelStudentModal}>학생 조회</Styled.RightItemLabel>
+                <Styled.RightItemLabel onClick={handleDeviceSettings}>
+                  디바이스 조회
+                </Styled.RightItemLabel>
+                <Styled.RightItemLabel onClick={handleDelStudentModal}>
+                  학생 조회
+                </Styled.RightItemLabel>
               </Styled.RightItem>
             ))}
           </Styled.RightContainer>
         </Styled.ListRightWrapper>
       </Styled.ListContainer>
-      
+
       <Styled.BarContainer>
         {/* Bar 컴포넌트 */}
         {filteredStudents.map((student) => (
